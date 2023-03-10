ca9ebec0f1f592b094a5f48c6bd2fb9ec752c825
BetterCallAle <dap22091998@gmail.com>
2023-03-10 14:06:02 +0100

    first commit

---
+++ src/org/lessons/java/shop/Products.java
@@ -0,0 +1,9 @@
+package org.lessons.java.shop;
+
+public class Products {
+    private int code;
+    private String name;
+    private String description;
+    private double price;
+    private int iva;
+}
